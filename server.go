4fb38c67c060590a96c745d2b52a470f13aef6a0
diko_ua <dlink.uaua@gmail.com>
2019-09-14 21:55:03 +0300

    added code to server

--- server.go
+++ server.go
@@ -22,3 +22,21 @@ func handleTime(responsewriter http.ResponseWriter, request *http.Request) {
   json.NewEncoder(responsewriter).Encode(datatosend)
 
 	}
+
+	func handleMain(responsewriter http.ResponseWriter, request *http.Request) {
+
+    responsewriter.Header().Set("Content-Type", "text/html")
+		responsewriter.WriteHeader(200)
+		responsewriter.Write([]byte("The main page\n"))
+		fmt.Println("/ page accessed");
+
+	}
+
+func main() {
+	http.HandleFunc("/", handleMain)
+	http.HandleFunc("/time", handleTime)
+
+	fmt.Println("Listening on 8795...");
+
+	log.Fatal(http.ListenAndServe(":8795", nil))
+}
