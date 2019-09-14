03149c87678f664007fc3081d2755d03aa767087
Daria Duminska <md.apathy@gmail.com>
2019-09-14 19:08:27 +0300

    Add handleTime function

--- server.go
+++ server.go
@@ -1,1 +1,24 @@
-added random text
+package main;
+
+import(
+	"fmt"
+	"log"
+	"time"
+	"encoding/json"
+	"net/http")
+
+type Time struct
+{
+	Time string
+}
+
+
+func handleTime(responsewriter http.ResponseWriter, request *http.Request) {
+
+	datatosend := Time{time.Now().Format(time.RFC3339)}
+
+  responsewriter.Header().Set("Content-Type", "application/json")
+	responsewriter.WriteHeader(200)
+  json.NewEncoder(responsewriter).Encode(datatosend)
+
+	}
