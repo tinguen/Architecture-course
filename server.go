ede038140412977ca8b83444b42abb4ad5daa9be
tinguen <tinguen@icloud.com>
2019-09-14 18:37:13 +0300

    Added handle function

--- server.go
+++ server.go
@@ -1,1 +1,23 @@
-added random text
+package main;
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type Time struct
+{
+	Time string
+}
+
+
+
+
+func handleMain(responsewriter http.ResponseWriter, request *http.Request) {
+	responsewriter.Header().Set("Content-Type", "text/html")
+		responsewriter.WriteHeader(200)
+		responsewriter.Write([]byte("The main page\n"))
+		fmt.Println("/ page accessed");
+}
+
+
